c20e01ffbd0da8e42e1eb5821a9c87dd83b58d05
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-22 09:54:55 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+namespace Uppgift_5._1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Skriv in 5 namn på nya rader");
+                string[] namnArray = new string[5];
+                for (int i = 0; i < namnArray.Length; i++)
+
+                {
+                    namnArray[i] = Console.ReadLine();
+                }
+                foreach (string namn in namnArray)
+                {
+                    Console.WriteLine(namn);
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Du måste ge rätt input, pröva igen");
+                Main();
+            }
+        }
+    }
+}
